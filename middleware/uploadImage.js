16525e93a7c44bf3901541a1cd17d13be05722ef
grienz <grienz@protonmail.com>
2022-03-12 06:11:12 +0300

    test_0.1

--- middleware/uploadImage.js
+++ middleware/uploadImage.js
@@ -5,16 +5,22 @@ module.exports = async function (req, res, next) {
     try {
         if(!req.files || Object.keys(req.files).length === 0)
             return res.status(400).json({msg: "No files were uploaded."})
+            
         const file = req.files.file;
 
 
-        console.log(file)
+        //console.log(file)
         // 1024 = 1 mb // 4096 = 4 mb
 
         if (file.size > 1024 * 1024 ){
             removeTmp(file.tempFilePath)
             return res.status(400).json({msg: "Size too LARGE."})
         }
+        if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png' && file.mimetype !== 'image/gif'){
+            removeTmp(file.tempFilePath)
+            return res.status(400).json({msg: "File format is incorrect."})
+        }
+
         next ()
     } catch (err) {
         return res.status(500).json({msg: err.message})
