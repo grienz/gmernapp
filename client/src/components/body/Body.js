d995e21fd90e343492b143e0571092acda9bccf5
grienz <grienz@protonmail.com>
2022-03-13 18:15:21 +0300

    test_0.1

--- client/src/components/body/Body.js
+++ client/src/components/body/Body.js
@@ -1,25 +1,3 @@
-/*import React from 'react'
-//"Switch" "component" changed => "Routes" "<Route path="/" element={<Home />} />element" 
-
-import {Routes, Route} from 'react-router-dom'
-import Login from './auth/Login'
-
-function Body() {
-  return (
-
-    
-      <section>
-        <Routes>
-           <Route path="/login" element={<Login />} /> 
-        </Routes>
-      </section>
-    
-    
-  )
-}
-
-export default Body*/
-
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./auth/Login";
--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -74,30 +74,3 @@ function Header() {
 }
 
 export default Header;
-
-/*
-import React from 'react'
-import {Link} from 'react-router-dom';
-
-function Header() {
-  return (
-
-    <header>
-
-        <div cloud_name="logo">
-            <h1><Link to="/"><i className="fa-solid fa-user-astronaut"></i> Grienz </Link></h1>
-        </div>
-
-        <ul>
-            <li><Link to="/login"><i className="fas fa-solid fa-user"></i> Login </Link></li>    
-            <li><Link to="/register"><i className="fa-solid fa-right-to-bracket"></i> Sign In </Link></li>
-        </ul>
-
-    </header>
-    
-  )
-}
-
-export default Header
-
-*/
